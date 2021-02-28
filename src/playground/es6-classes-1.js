ace967112f88986bb6a23a983bec940619fa70e1
gpender <guy.pender@me.com>
2021-02-28 22:24:52 +0000

    Added Super Class

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -12,9 +12,46 @@ class Person{
     }
 
 }
+class Student extends Person {
+
+    constructor(name,age,major){
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor(){
+        return !!this.major;
+    }
+    getDescription(){
+        let description = super.getDescription();
+        if(this.hasMajor){
+            description += `. Their major is ${this.major}.`;
+        }
+        return description;
+    }
+}
+
+class Traveller extends Person{
+    constructor(name,age,homeLocation){
+        super(name,age);
+        this.homeLocation = homeLocation;
+
+    }
+    getGreeting(){
+        let greeting = super.getGreeting();
+        if(this.homeLocation){
+            greeting += `. I'm visiting from ${this.homeLocation}.`;
+        }
+        return greeting;
+    }
+}
 
 const me = new Person('Guy Pender', 54);
-const other = new Person();
+const other = new Student('Student Trev',99,'Bsc');
 
 console.log(me.getGreeting());
 console.log(me.getDescription());
+console.log(other.hasMajor());
+console.log(other.getDescription());
+
+const traveller = new Traveller('Trevor',66,'Scilly');
+console.log(traveller.getGreeting());
