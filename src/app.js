251c3e0461fbca610033d64f65996a4eb9d3a20e
gpender <guy.pender@me.com>
2021-03-06 16:49:46 +0000

    Added React and ReactDOM modules

--- src/app.js
+++ src/app.js
@@ -1,15 +1,6 @@
-import subtract, {square, add} from './utils';
-// import {square, add} from './utils';
-import isSenior, {isAdult, canDrink} from './person';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is running!!');
-console.log(square(4));
-console.log(add(4,45));
-console.log(subtract(5,6));
+const template = React.cloneElement('p',{},'testing');
 
-console.log(isAdult(18));
-console.log(isAdult(17));
-console.log(canDrink(18));
-console.log(canDrink(21));
-
-console.log(`Is senior : ${isSenior(6)}`);
+ReactDOM.render(template, document.getElementById('app'));
