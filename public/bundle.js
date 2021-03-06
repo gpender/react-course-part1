0b774516f2cf3f7655bd1455adff9c50d03405c6
gpender <guy.pender@me.com>
2021-03-06 16:50:27 +0000

    resolve bundle.js

--- public/bundle.js
+++ public/bundle.js
@@ -63,14 +63,15 @@
 /******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
+/******/ ({
+
+/***/ 0:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__person__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils__ = __webpack_require__(35);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__person__ = __webpack_require__(36);
 
 // import {square, add} from './utils';
 
@@ -88,7 +89,8 @@ console.log(__WEBPACK_IMPORTED_MODULE_1__person__["a" /* canDrink */](21));
 console.log(`Is senior : ${__WEBPACK_IMPORTED_MODULE_1__person__["b" /* default */](6)}`);
 
 /***/ }),
-/* 1 */
+
+/***/ 35:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -111,7 +113,8 @@ const subtract = (a,b) => a - b;
 
 
 /***/ }),
-/* 2 */
+
+/***/ 36:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -127,4 +130,5 @@ const canDrink = (age) => age>=21;
 
 
 /***/ })
-/******/ ]);
+
+/******/ });
