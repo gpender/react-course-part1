a46a7ca52ccab993daa5d282ef98156563c644b0
gpender <guy.pender@me.com>
2021-03-06 16:04:10 +0000

    Added new js files and referenced functions using Import/Export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+//import './utils';
+import {square, add} from './utils';
+import {isAdult, canDrink} from './person';
+
 console.log('app.js is running!!');
+console.log(square(4));
+console.log(add(4,45));
+
+console.log(isAdult(18));
+console.log(isAdult(17));
+console.log(canDrink(18));
+console.log(canDrink(21));
