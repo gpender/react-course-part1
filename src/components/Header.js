6162697b8b72fc7a68b5fbc2f1af380ee0dd7b53
gpender <guy.pender@me.com>
2021-03-07 17:33:31 +0000

    Started styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Header =(props) => 
 (
-    <div>
-        <h1>{props.title}</h1>
-        {props.subTitle && <h2>{props.subTitle}</h2>}
+    <div className="header">
+        <h1 class="header__title">{props.title}</h1>
+        {props.subTitle && <h2 className="header__subtitle">{props.subTitle}</h2>}
     </div>
 )
 
