e87f467913399c4e07e141386107af5d77ae8964
gpender <guy.pender@me.com>
2021-03-13 18:48:05 +0000

    Add Modal Component Styling

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,13 @@ const OptionModal = (props) =>
     <Modal
         isOpen={!!props.selectedOption}
         contentLabel="Selected Option"
-        onRequestClose={props.onPickOk}>
-        <h3>Selected Modal</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.onPickOk}>Ok</button>
+        onRequestClose={props.onPickOk}
+        closeTimeoutMS={200}
+        className='modal'
+        >
+        <h3 className='modal__title'>Selected Option</h3>
+        {props.selectedOption && <p className='modal__body'>{props.selectedOption}</p>}
+        <button className='button' onClick={props.onPickOk}>Ok</button>
     </Modal>
 );
 
